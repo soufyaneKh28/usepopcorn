395b9ecba5a19535341185996efe080204b97111
soufyane khalfallah <khalfalasoufi@gmail.com>
2024-04-02 11:39:52 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -8,7 +8,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      size={35}
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
     {/* <StarRating maxRating={10} />
     <StarRating /> */}
   </React.StrictMode>
